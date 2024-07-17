1f5f162f2d245e52dd0af36b0b348cc2b32f31e5
Inho Oh <webispy@gmail.com>
2024-07-17 07:09:17 +0000

    aln: update 0.1.1 bottle.

--- Formula/aln.rb
+++ Formula/aln.rb
@@ -8,10 +8,10 @@ class Aln < Formula
   head "https://github.com/webispy/aln.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/webispy/homebrew-aln/releases/download/aln-0.1.0"
-    sha256 cellar: :any, arm64_sonoma: "a7f35e8d97f0181b7e689193ce724a74ebb808b7a82c1981571a212ad51b9c51"
-    sha256 cellar: :any, ventura:      "820cb671adf921665481cbedfba52e6a2bf0fc7abf96e34ceecec45556fafea6"
-    sha256 cellar: :any, monterey:     "c16fd2d7a9caa6ccc3fac7d5f980613467602a8067f6c8a05f8b36ced37fb7bc"
+    root_url "https://github.com/webispy/homebrew-aln/releases/download/aln-0.1.1"
+    sha256 cellar: :any, arm64_sonoma: "8409d4618bae43c9ccd97889d16daacee1f2ab3b767b35afbbdf2e353c988094"
+    sha256 cellar: :any, ventura:      "16fbe744f0b1d481d7d65f758251e06ff763aaf8e3c09797e02ed657b0a9a767"
+    sha256 cellar: :any, monterey:     "a161fc7d7a7a4b8b7eabb5a996393c590e4f5d5ca35440578b49f78897b0692e"
   end
 
   depends_on "cmake" => :build
